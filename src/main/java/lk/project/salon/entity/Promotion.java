03b09459ae2e97a87a48582ddf18bf7c60b754c4
Ramesh Kaushika De Silva <ramesh.kaushika2245@gmail.com>
2024-08-30 01:58:33 +0530

    Getter & Setter , Constructors Called
    -2024-08-30-

--- src/main/java/lk/project/salon/entity/Promotion.java
+++ src/main/java/lk/project/salon/entity/Promotion.java
@@ -1,7 +1,11 @@
 package lk.project.salon.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.ToString;
 
+@Getter
+@ToString
 @Entity
 public class Promotion {
     @Id
@@ -17,4 +21,42 @@ public class Promotion {
     private String endDate;
     @Column(nullable = false)
     private float discount;
+
+    public Promotion() {
+    }
+
+    public Promotion(int promotionId, String title, String description, String startDate, String endDate, float discount) {
+        this.promotionId = promotionId;
+        this.title = title;
+        this.description = description;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.discount = discount;
+    }
+
+    public void setPromotionId(int promotionId) {
+        this.promotionId = promotionId;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public void setDiscount(float discount) {
+        this.discount = discount;
+    }
+
+
 }
