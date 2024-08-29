fc35c97beee85dad186366f126cd8f4572016445
Ramesh Kaushika De Silva <ramesh.kaushika2245@gmail.com>
2024-08-30 01:58:35 +0530

    Getter & Setter , Constructors Called
    -2024-08-30-

--- src/main/java/lk/project/salon/entity/Service.java
+++ src/main/java/lk/project/salon/entity/Service.java
@@ -1,8 +1,16 @@
 package lk.project.salon.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+@Getter
+@ToString
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Service {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,4 +23,25 @@ public class Service {
     private float price;
     @Column(nullable = false)
     private String duration;
+
+    public void setServiceId(int serviceId) {
+        this.serviceId = serviceId;
+    }
+
+    public void setServiceName(String serviceName) {
+        this.serviceName = serviceName;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
 }
+
