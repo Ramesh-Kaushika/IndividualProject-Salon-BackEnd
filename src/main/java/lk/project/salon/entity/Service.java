833790e497862efef62413879b09e9e341941781
Ramesh Kaushika De Silva <ramesh.kaushika2245@gmail.com>
2024-08-30 01:34:16 +0530

    Service Table Created
    -2024-08-30-

---
+++ src/main/java/lk/project/salon/entity/Service.java
@@ -0,0 +1,18 @@
+package lk.project.salon.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Service {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int serviceId;
+    @Column(nullable = false)
+    private String serviceName;
+    @Column(nullable = false)
+    private String description;
+    @Column(nullable = false)
+    private float price;
+    @Column(nullable = false)
+    private int duration;
+}
